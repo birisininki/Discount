ebb582769d646eea2a7111c4dd237f1d6c80fadf
JIM SINATRA <jimsinatra30@gmail.com>
2022-04-03 21:44:25 +0200

    refresh

--- resources/views/layouts/front/head.blade.php
+++ resources/views/layouts/front/head.blade.php
@@ -35,5 +35,6 @@
 <link rel="stylesheet" href="{{asset('frontend/assets/style/swiper.min.css')}}" />
 <link rel="stylesheet" href="{{asset('frontend/assets/style/main.css')}}">
 <link rel="stylesheet" href="{{asset('frontend/assets/style/discount.css')}}">
-<link rel="stylesheet" href="{{asset('frontend/assets/style/responsive.css')}}">  
+<link rel="stylesheet" href="{{asset('frontend/assets/style/responsive.css')}}">
+<meta http-equiv="refresh" content="15; URL={{route('home')}}" />  
 <body>
